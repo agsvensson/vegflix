a4bdf2f56bbdff7bf0fe07b921886b87b8834092
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2020-08-03 00:25:26 -0300

    Ajuste links e pastas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+// este arquivo é o que vai inicializar o React
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // ao redor da aplicação, tem que ter um roteamento, é o que o BrowserRouter faz.
+  // Switch é como se tivesse um if dentro para verificar qual rota é.
+  // Route é como vamos declarar cada uma das páginas que a gente tem.
+  <BrowserRouter>
+  <Switch> 
+  <Route path="/" component={App} />
+
+  </Switch> 
+  </BrowserRouter>,
+  document.getElementById('root') // pega e renderiza o React dentro do root
 );
